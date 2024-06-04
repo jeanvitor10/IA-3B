a9dc42aac6ddcf65a14bc8983682a7e69527727a
jeanvitor10 <111467996+jeanvitor10@users.noreply.github.com>
2024-06-04 12:08:58 -0300

    jj

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const caixaPricincipal = document.querySelector(".caixa-pricipal")
-const caixaPricincipal = document.querySelector(".caixa-pricipal")
-const caixaPricincipal = document.querySelector(".caixa-alternativa")
-const caixaPricincipal = document.querySelector(".caixa-resultado")
-const caixaPricincipal = document.querySelector(".caixa-reultado")
+const caixaPrincipal = document.querySelector(".caixa-pricipal")
+const caixaPergunta = document.querySelector(".caixa-pricipal")
+const caixaalternativa = document.querySelector(".caixa-alternativa")
+const caixaresultado = document.querySelector(".caixa-resultado")
+const caixaresultado = document.querySelector(".caixa-reultado")
+
+const perguntas = [
+    {///primeiro obejeto da lista de perguntas
+    enunciado: "perguntal" , 
+    alternativa: ["alternatival","alternativa2"]
+},
+   {//segundo obejetivo da lista de perguntas
+   enunciado: "pergunta2",
+   alternativa: ["alternativa", "alternativa2"]
+
+   }
+];
